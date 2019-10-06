35d85f10f3623b0281bd8d6c869ae0af4a4c8425
Saikat Roy <saikatkrroy@gmail.com>
2019-10-06 22:31:18 +1100

    Project Created

---
+++ Roulette/Roulette.DataAccess/RouletteDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roulette.DataAccess
+{
+    public class RouletteDBContext: DbContext
+    {
+        public RouletteDBContext()
+        {
+
+        }
+    }
+}
