819e667d1a145f49dfe182fc71dd5337d7e0db6c
Saikat Roy <saikatkrroy@gmail.com>
2019-10-15 18:03:22 +1100

    Javascript and backend working properly

--- Roulette/Roulette/Scripts/Index.js
+++ Roulette/Roulette/Scripts/Index.js
@@ -9,7 +9,6 @@ app.controller("HomeController", function ($scope, $http) {
     $scope.ColorStats = [];
     $scope.BetPlaced = false;
     $scope.ActionFailed = false;
-
     $http.get('/api/RouletteEntry/RetrieveNumbers')
         .then(function successCallback(response) {
             $scope.Numbers = JSON.parse(JSON.stringify(response.data));
@@ -17,14 +16,14 @@ app.controller("HomeController", function ($scope, $http) {
             function failureCallback(response) {
                 var data = response.Data;
             });
-},
-    this.PlaceYourBet = function ($scope, $http) {
-        $http.post('/api/RouletteEntry/CreateUserInput', bet).then(
+    PlaceYourBet = function () {
+        $http.post('/api/RouletteEntry/CreateUserInput', $scope.bet).then(
             function successCallback(response) {
-                if (response.status == 200)
+                if (response.status == 204)
                     $scope.BetPlaced = true;
             },
             function failureCallback(response) {
                 $scope.ActionFailed = true;
             });
+    }
 });
