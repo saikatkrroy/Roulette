b987e0d1b490737426336d1f7d48fdc95fb8006a
saikatkrroy <38486474+saikatkrroy@users.noreply.github.com>
2020-03-29 11:14:14 +1100

    Syntax error change

--- Roulette/Roulette.DataAccess/Migrations/202003060034193_Update Roulette EventName.cs
+++ Roulette/Roulette.DataAccess/Migrations/202003060034193_Update Roulette EventName.cs
@@ -7,10 +7,10 @@ namespace Roulette.DataAccess.Migrations
     {
         public override void Up()
         {
-            Sql("Update [Roulette].[RouletteEvents] set EventName='RA 01 (Min 2 \u20AC Max 20 \u20AC') where EventName='RA 01'");
-            Sql("Update [Roulette].[RouletteEvents] set EventName='RA 02 (Min 5 \u20AC Max 50 \u20AC') where EventName='RA 02'");
-            Sql("Update [Roulette].[RouletteEvents] set EventName='RA 11 (Min 2 \u20AC Max 20 \u20AC') where EventName='RA 11'");
-            Sql("Update [Roulette].[RouletteEvents] set EventName='RA 12 (Min 5 \u20AC Max 50 \u20AC') where EventName='RA 12'");
+            Sql("Update [Roulette].[RouletteEvents] set EventName='RA 01 (Min 2 \u20AC Max 20 \u20AC)' where EventName='RA 01'");
+            Sql("Update [Roulette].[RouletteEvents] set EventName='RA 02 (Min 5 \u20AC Max 50 \u20AC)' where EventName='RA 02'");
+            Sql("Update [Roulette].[RouletteEvents] set EventName='RA 11 (Min 2 \u20AC Max 20 \u20AC)' where EventName='RA 11'");
+            Sql("Update [Roulette].[RouletteEvents] set EventName='RA 12 (Min 5 \u20AC Max 50 \u20AC)' where EventName='RA 12'");
         }
         
         public override void Down()
